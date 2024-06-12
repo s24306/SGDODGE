f3c34c8281d140d745aad4222adbc3e32a2c3c75
s24306 <s24306@pjwstk.edu.pl>
2024-06-12 16:11:03 +0200

    Split walking anim into four corners

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "libs/SDL2/include/SDL.h"
-#include "libs/SDL2_image/include/SDL_image.h"
+#include "Classes/LTexture.h"
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
@@ -12,6 +12,12 @@ void close();
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gImage = NULL;
+SDL_Surface* gCurrentSurface = NULL;
+
+SDL_Rect gSpriteClips[ 4 ];
+LTexture gSpriteSheetTexture;
+
+
 
 bool init(){
     bool success = true;
@@ -19,33 +25,133 @@ bool init(){
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
         success = false;
     } else {
-        gWindow = SDL_CreateWindow("SGRUN", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        gWindow = SDL_CreateWindow("SGRUN", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if(gWindow == NULL){
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
             success = false;
-        } else {
-                gScreenSurface = SDL_GetWindowSurface(gWindow);
-            }
+        } else{
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+			}
+        }
     }
     return success;
 }
 
-bool loadMedia(){
-    bool success = true;
-    gImage = SDL_LoadBMP("img/idle.bmp");
-    if(gImage == NULL){
-        printf( "Unable to load image %s! SDL Error: %s\n", "img/idle.png", SDL_GetError() );
-        success = false;
+SDL_Surface* loadSurface( std::string path )
+{
+    SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
     }
-    return success;
+
+    return loadedSurface;
+}
+
+bool LTexture::loadFromFile( std::string path )
+{
+	//Get rid of preexisting texture
+	free();
+
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = loadSurface( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), SDL_GetError() );
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load sprite sheet texture
+	if( !gSpriteSheetTexture.loadFromFile( "img/walking_sprite.bmp" ) )
+	{
+		printf( "Failed to load sprite sheet texture!\n" );
+		success = false;
+	}
+	else
+	{
+		//Set first sprite
+		gSpriteClips[ 0 ].x =   0;
+		gSpriteClips[ 0 ].y =   0;
+		gSpriteClips[ 0 ].w = gSpriteSheetTexture.getWidth()/4;
+		gSpriteClips[ 0 ].h = gSpriteSheetTexture.getHeight();
+
+		//Set second sprite
+		gSpriteClips[ 1 ].x = gSpriteSheetTexture.getWidth()/4;
+		gSpriteClips[ 1 ].y =   0;
+		gSpriteClips[ 1 ].w = gSpriteSheetTexture.getWidth()/4;
+		gSpriteClips[ 1 ].h = gSpriteSheetTexture.getHeight();
+		
+		//Set third sprite
+		gSpriteClips[ 2 ].x = (gSpriteSheetTexture.getWidth()/4)*2;
+		gSpriteClips[ 2 ].y = 0;
+		gSpriteClips[ 2 ].w = gSpriteSheetTexture.getWidth()/4;
+		gSpriteClips[ 2 ].h = gSpriteSheetTexture.getHeight();
+
+		//Set fourth sprite
+		gSpriteClips[ 3 ].x = (gSpriteSheetTexture.getWidth()/4)*3;
+		gSpriteClips[ 3 ].y = 0;
+		gSpriteClips[ 3 ].w = gSpriteSheetTexture.getWidth()/4;
+		gSpriteClips[ 3 ].h = gSpriteSheetTexture.getHeight();
+	}
+
+	return success;
 }
 
 void close(){
+
+    //Free loaded images
+	gSpriteSheetTexture.free();
+
+	//Destroy window	
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
     SDL_FreeSurface(gImage);
     gImage = NULL;
-
-    SDL_DestroyWindow( gWindow );
-    gWindow = NULL;
+	gWindow = NULL;
+	gRenderer = NULL;
 
     SDL_Quit();
 }
@@ -64,10 +170,28 @@ int main( int argc, char* args[] ){
                 while( SDL_PollEvent( &e ) ){
                     if( e.type == SDL_QUIT ){
                         quit = true;
+                    } else if(e.type == SDL_QUIT){
+
                     }
                 }
-                SDL_BlitSurface(gImage, NULL, gScreenSurface, NULL);
-                SDL_UpdateWindowSurface( gWindow );
+                //Clear screen
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_RenderClear( gRenderer );
+
+				//Render top left sprite
+				gSpriteSheetTexture.render( 0, 0, &gSpriteClips[ 0 ] );
+
+				//Render top right sprite
+				gSpriteSheetTexture.render( SCREEN_WIDTH - gSpriteClips[ 1 ].w, 0, &gSpriteClips[ 1 ] );
+
+				//Render bottom left sprite
+				gSpriteSheetTexture.render( 0, SCREEN_HEIGHT - gSpriteClips[ 2 ].h, &gSpriteClips[ 2 ] );
+
+				//Render bottom right sprite
+				gSpriteSheetTexture.render( SCREEN_WIDTH - gSpriteClips[ 3 ].w, SCREEN_HEIGHT - gSpriteClips[ 3 ].h, &gSpriteClips[ 3 ] );
+
+				//Update screen
+				SDL_RenderPresent( gRenderer );
             }
         }
     }
